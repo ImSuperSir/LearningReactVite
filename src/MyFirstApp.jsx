7eb2f0ae799cd332684c63056d93a0c79b91437f
ImsuperSir <imsupersir@gmail.com>
2023-12-07 16:08:19 -0800

    MyFirstComponent - llamandoComponentes

--- src/MyFirstApp.jsx
+++ src/MyFirstApp.jsx
@@ -1,8 +1,32 @@
 //Esta es mi primera aplicacion
 
-export const MyFirstApp = () => (
-<>
-<h1>Esta es mi primera aplicación</h1>
-<p>Este es un segundo elemento</p>
-</>
-);
+const GetUserName = () =>
+{
+  return 'Ramirez está viviendo en Vancouver BC, Canada.';
+};
+
+const getSumaNumeros = (a, b) =>
+{
+  return (a + b);
+}
+
+
+// export const MyFirstApp = () => (
+// <>
+// <h1>Esta es mi primera aplicación</h1>
+// <p>Este es un segundo elemento</p>
+// </>
+// );
+
+export const MyFirstApp = () =>
+{
+  const lValor = GetUserName();
+  return (
+    <>
+      {
+  /*Aca puedo tranquilamente agregar comentarios  */}
+      <h1>{lValor}</h1>
+      <h2>{getSumaNumeros(2, 5)}</h2>
+    </>
+  )
+}
