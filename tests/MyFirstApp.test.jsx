cc2f24e2b24f8dd40c1dbb0b2a298a1620f3d860
ImsuperSir <imsupersir@gmail.com>
2023-12-12 00:27:20 -0800

    Testing - ReactTestingLibrary-Snapshot - FirstTestOnRenderContainer

--- tests/MyFirstApp.test.jsx
+++ tests/MyFirstApp.test.jsx
@@ -5,8 +5,28 @@ describe('Test on <MyFirstApp />', () => {
   
   test('should match with the snapshot', () => {
     
-    const lTitle = 'Frida Sophia Ramirez';
-    render(<MyFirstApp title={lTitle} />);
+    const lTitle = 'Frida Sophia';
+    const {container} =  render(<MyFirstApp title={lTitle} />);
+
+    expect(container).toMatchSnapshot();
+
+
+
   });
+
+  test('should show display the title into a h1 label', () => {
+
+    const lTitle = 'Frida Sophia';
+    const {container, getByText} = render(<MyFirstApp title={lTitle}/>);
+
+    expect(getByText(lTitle)).toBeTruthy();
+
+    const lH1 = container.querySelector('h1');
+    // console.log(lH1.innerHTML);
+    expect(lH1.innerHTML).toBe(lTitle);
+    expect(lH1.innerHTML).toContain(lTitle);
+    
+  })
+  
 
 });
